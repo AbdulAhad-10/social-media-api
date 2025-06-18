dc7dbef280b4639b17a5b250be9fc04a2c02816c
Abdul Ahad <ahad55056@gmail.com>
2025-06-18 09:52:04 +0500

    implemented user controller

---
+++ controllers/user.controller.js
@@ -0,0 +1,68 @@
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+
+export const getUserDetails = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password -__v");
+    if (!user) {
+      const error = new Error("User not found");
+      error.status = 404;
+      throw error;
+    }
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const updateUserDetails = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      {
+        _id: req.user._id,
+      },
+      req.body,
+      { new: true, runValidators: true }
+    ).select("-password -__v");
+    if (!user) {
+      const error = new Error(
+        "User not found or you do not have permission to update it"
+      );
+      error.status = 404;
+      throw error;
+    }
+    res.status(200).json({
+      success: true,
+      message: "User details updated successfully",
+      data: user,
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const getUserPosts = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id).select("-password -__v");
+    if (!user) {
+      const error = new Error("User not found");
+      error.status = 404;
+      throw error;
+    }
+
+    const posts = await Post.find({ userId: req.user._id }).select("-__v");
+
+    res.status(200).json({
+      success: true,
+      data: {
+        user,
+        posts,
+      },
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,17 +1,17 @@
 import { Router } from "express";
+import authorize from "../middleware/auth.middleware.js";
+import {
+  getUserDetails,
+  getUserPosts,
+  updateUserDetails,
+} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.get("/me", (req, res) => {
-  res.send("User Profile Details");
-});
+userRouter.get("/me", authorize, getUserDetails);
 
-userRouter.put("/me", (req, res) => {
-  res.send("User Profile Updated");
-});
+userRouter.put("/me", authorize, updateUserDetails);
 
-userRouter.get("/me/posts", (req, res) => {
-  res.send("List of User Posts");
-});
+userRouter.get("/me/posts", authorize, getUserPosts);
 
 export default userRouter;
